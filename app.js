5a1db9589ec2b65f89bf2becf8fe9a928d51391b
Krstevskii <mr.krstevski@live.com>
2019-02-16 20:14:55 +0100

    update bike edit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const moment = require('moment-timezone');
 
 const port = process.env.PORT || 4000;
 
-mongoose.connect('mongodb+srv://krstevskii:imwIqflnyK6YEWuP@arduino-express-nfc-lv454.mongodb.net/test?retryWrites=true', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://krstevskii:imwIqflnyK6YEWuP@arduino-express-nfc-lv454.mongodb.net/bikes?retryWrites=true', {useNewUrlParser: true})
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
@@ -44,72 +44,74 @@ app.get('/', (req, res) => {
 app.post('/pay', ensureEndString, (req, res) => {
 
     const Pay = {
-        embg: req.body.embg,
+        embg: req.body.bike_id,
         pay_part1: req.body.extra,
+        onStation: req.body.onStation
     };
 
-    CBike.findOne({embg: Pay.embg})
-        .then(cuser => {
-            cuser.endTime = Date.now();
-
-            cuser
-                .save()
-                .then(endUser => {
-
-                    const payTimeDifference = moment.utc(moment(endUser.endTime, "DD/MM/YYYY HH:mm:ss").diff(moment(endUser.startTime, "DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
-                    timeHMSArray = payTimeDifference.split(':');
-
-                    for (let i = 0; i < timeHMSArray.length; i++) {
-                        timeHMSArray[i] = parseInt(timeHMSArray[i]) / Math.pow(60, i);
-                    }
-
-                    const totalTime = Math.ceil(timeHMSArray.reduce((total, current) => total + current)) * 30;
-                    const finalPrice = totalTime + parseInt(Pay.pay_part1);
-
-                    User.findOne({embg: endUser.embg})
-                        .then(user => {
-                            user.credits = user.credits - finalPrice;
-                            user.save()
-                                .then(user => {
-                                    // res.send('The price has been deducted');
-                                    CBike.findOne({embg: user.embg}, {_id: 0})
-                                        .then(user => {
-                                            console.log(user);
-                                            const newUserToPermSave = {
-                                                embg: user.embg,
-                                                bike_id: user.bike_id,
-                                                startTime: user.startTime,
-                                                longitude: user.longitude,
-                                                latitude: user.latitude,
-                                                endTime: user.endTime
-                                            };
-                                            new PastBike(newUserToPermSave)
-                                                .save()
-                                                .then(user => {
-
-                                                    CBike.remove({embg: user.embg})
-                                                        .then(() => res.send('The price has been deducted'))
-                                                        .catch(err => console.log(err));
-
-                                                })
-                                                .catch(err => console.log(err));
-                                        });
-                                })
-                                .catch(() => console.log('The user has not been saved'));
-                        })
-                        .catch(() => console.log(`The user doesn't exist`));
-
-                });
-        });
+    // CBike.findOne({embg: Pay.embg})
+    //     .then(cuser => {
+    //         cuser.endTime = Date.now();
+    //
+    //         cuser
+    //             .save()
+    //             .then(endUser => {
+    //
+    //                 const payTimeDifference = moment.utc(moment(endUser.endTime, "DD/MM/YYYY HH:mm:ss").diff(moment(endUser.startTime, "DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss");
+    //                 timeHMSArray = payTimeDifference.split(':');
+    //
+    //                 for (let i = 0; i < timeHMSArray.length; i++) {
+    //                     timeHMSArray[i] = parseInt(timeHMSArray[i]) / Math.pow(60, i);
+    //                 }
+    //
+    //                 const totalTime = Math.ceil(timeHMSArray.reduce((total, current) => total + current)) * 30;
+    //                 const finalPrice = totalTime + parseInt(Pay.pay_part1);
+    //
+    //                 User.findOne({embg: endUser.embg})
+    //                     .then(user => {
+    //                         user.credits = user.credits - finalPrice;
+    //                         user.save()
+    //                             .then(user => {
+    //                                 // res.send('The price has been deducted');
+    //                                 CBike.findOne({embg: user.embg}, {_id: 0})
+    //                                     .then(user => {
+    //                                         console.log(user);
+    //                                         const newUserToPermSave = {
+    //                                             embg: user.embg,
+    //                                             bike_id: user.bike_id,
+    //                                             startTime: user.startTime,
+    //                                             longitude: user.longitude,
+    //                                             latitude: user.latitude,
+    //                                             endTime: user.endTime
+    //                                         };
+    //                                         new PastBike(newUserToPermSave)
+    //                                             .save()
+    //                                             .then(user => {
+    //
+    //                                                 CBike.remove({embg: user.embg})
+    //                                                     .then(() => res.send('The price has been deducted'))
+    //                                                     .catch(err => console.log(err));
+    //
+    //                                             })
+    //                                             .catch(err => console.log(err));
+    //                                     });
+    //                             })
+    //                             .catch(() => console.log('The user has not been saved'));
+    //                     })
+    //                     .catch(() => console.log(`The user doesn't exist`));
+    //
+    //             });
+    //     });
 });
 app.post('/update_bike_user', ensureEndString, (req, res) => {
 
     let latitudeUpdate = req.body.latitude / Math.pow(10, 6);
     let longitudeUpdate = req.body.longitude / Math.pow(10, 6);
     let embg = req.body.embg;
+    let bike_id = req.body.bike_id
 
     CBike.updateOne(
-        {embg: embg},
+        {bike_id: bike_id},
         {$push: {longitude: longitudeUpdate, latitude: latitudeUpdate}})
         .then(result => res.send("Map is Updated"))
         .catch(err => res.status(503).send("An error occurred"));
@@ -133,14 +135,14 @@ app.post('/start_bike_user', ensureEndString, (req, res) => {
                     .save()
                     .then(bikeUserModel => res.send("The bike has started"))
                     .catch(err => res.status(503).send("An error occurred"));
-
             } else {
                 if( result.embg === BuildUserBikeModel.embg )
                     res.send('The user has already started the bike');
                 else
                     res.send('Another user already uses the bike');
             }
-        });
+        })
+        .catch(err => res.status(503).send("An error occurred"));
 });
 
 app.post('/find_user', ensureEndString, (req, res) => {
