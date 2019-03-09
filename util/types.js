fcc5a87a45461a242b2cb1c102e57402e3620cb9
Krstevskii <mr.krstevski@live.com>
2019-03-09 18:22:38 +0100

    type checking utility upgraded

--- util/types.js
+++ util/types.js
@@ -1,25 +1,24 @@
 module.exports = (data) => {
     switch (typeof data) {
         case 'object':
-            const typeObject = {};
-            if (Object.keys('data').length !== 0) {
+            if (Array.isArray(data) && data.length !== 0) {
+                const typeArray = {};
+                if (data.length !== 0) {
+                    for (let i in data) {
+                        typeArray[`${data[i]}`] = typeof data[i];
+                    }
+                    return typeArray;
+                }
+            } else if (Object.keys('data').length !== 0) {
+                const typeObject = {};
                 for (let i in data) {
                     typeObject[i] = typeof data[i];
                 }
                 return typeObject;
             }
             break;
-        case 'array':
-            const typeArray = {};
-            if (data.length !== 0) {
-                for (let i in data) {
-                    typeArray[data[i]] = typeof data[i];
-                }
-                return typeArray;
-            }
-            break;
         default:
-            return data === null ? null : typeof data;
+            return data === null ? null : data.toString() + ': ' + typeof data;
 
     }
 };
